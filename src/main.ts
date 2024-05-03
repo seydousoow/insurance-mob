476760babdd86c733b05d151320e84e702b8bdad
Seydou.Sow <seydousoow@gmail.com>
2024-05-03 12:01:21 +0200

    init add tailwindcss fix config issue use standalone

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { enableProdMode } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {enableProdMode} from '@angular/core';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {provideRouter, RouteReuseStrategy} from '@angular/router';
+import {IonicRouteStrategy, provideIonicAngular} from '@ionic/angular/standalone';
 
-import { routes } from './app/app.routes';
-import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
+import {routes} from './app/app.routes';
+import {AppComponent} from './app/app.component';
+import {environment} from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
@@ -13,8 +13,8 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
+    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+    provideIonicAngular({mode: 'ios'}),
     provideRouter(routes),
   ],
 });
